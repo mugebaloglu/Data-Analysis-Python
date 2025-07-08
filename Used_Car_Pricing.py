78c32e8d494937a8d14584f07ffee2cedfa49560
Muge Baloglu <mugeebaloglu@gmail.com>
2025-07-08 20:31:23 +0300

    laptop pricing in python

--- Used_Car_Pricing.py
+++ Used_Car_Pricing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+
 filepath = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
 df = pd.read_csv(filepath, header=None)
 
